28fc3310c819ad9c3dcfdf8ff5d21239f1b8a433
Omer Hadad <omerhadad10@gmail.com>
2024-09-10 00:25:50 +0300

    Small fixes

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
 import subprocess
+import os
 
 app = Flask(__name__)
-WAKE_ON_LAN_SHELL_PATH = './shells/wake_on_lan_shell.sh'
+WAKE_ON_LAN_SHELL_PATH = './shells/wake_on_lan.sh'
 TIMEOUT = '30'
-
+PORT = os.getenv('APP_PORT', 5000)
 
 @app.route('/wake', methods=['POST'])
 def wake_device():
@@ -21,13 +22,16 @@ def wake_device():
             stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
         output = result.stdout.decode('utf-8')
+        log_array = output.split('\n')
+        log_array = [line for line in log_array if line]
+
         exit_code = result.returncode
 
-        return jsonify({"output": output, "exit_code": exit_code}), 200
+        return jsonify({"output": log_array, "exit_code": exit_code}), 200
     except Exception as e:
         error_message = f'Error {str(e)}'
         return error_message, 500
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=PORT)
